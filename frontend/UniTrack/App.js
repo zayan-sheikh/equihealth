727fc498d4c4a9d983bc999f28512800c1018975
Chloe Zandberg <chloezandberg@dhcp-128-189-193-85.ubcsecure.wireless.ubc.ca>
2024-04-06 16:22:24 -0700

    fixed what I broke? maybe?

---
+++ frontend/UniTrack/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.userText}> Hello, User!</Text>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: '#4ECDC4',
+    padding: 20,
+  },
+  userText: {
+    textSize: 20,
+    textAlign: 'left',
+  },
+});
