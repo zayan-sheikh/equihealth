667fd2d19fe6027dd7ef51183cd00d4c3ba44549
Andrew Ahn <andrewahn21@gmail.com>
2024-04-06 23:21:35 -0700

    d

--- frontend_real/src/pages/Home.js
+++ frontend_real/src/pages/Home.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Home.css";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Home() {
+    const [streak, setStreak] = useState(-1)
+
+    useEffect(() => {
+        const fetchStreak = async () => {
+            const response = await fetch('/api/main')
+            const json = await response.json()
+            console.log(json)
+
+            if (response.ok) {
+                setStreak(json.streak)
+            }
+        }
+
+        fetchStreak()
+    }, [])
+
     return (
-        <div class="test">
+        <div className="test">
         
-        <div class="container">
-            <div class="header">
-                <p class="userText"> Hello, User!</p>
+        <div className="container">
+            <h1>{streak}</h1>
+
+            <div className="header">
+                <p className="userText"> Hello, User!</p>
             </div>
 
-            <div class="header">
-                <Link to='/tasks' class="userText">View Daily Task</Link>
+            <div className="header">
+                <Link to='/tasks' className="userText">View Daily Task</Link>
             </div>
         </div>
 
