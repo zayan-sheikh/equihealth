2822bd8ccc4bbe4484bc109c45f3a6f2fdaffa64
zayan-sheikh <dbzayan@gmail.com>
2024-04-06 23:22:06 -0700

    Added stats page

---
+++ frontend_real/src/pages/Stats.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Stats.css";
+import {Link} from 'react-router-dom';
+
+function Stats() {
+    return (
+        <div class="container">
+
+        <div class="Back"></div>
+        
+        <div class="Label"></div>
+
+        <div class="Graph"></div>
+
+        <div class="Priority"></div>
+
+        <div class="Balance"></div>
+
+        <div class="Plug"></div>
+
+        <div class="Leaderboard"></div>
+
+        </div>
+    )
+};
+
+
+
+
+
+
+export default Stats
